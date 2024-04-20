89b77134e89288c9eed8a637e6b94ed8289df2d0
PauloMBorges <paulodemeloborges@gmail.com>
2024-04-19 22:23:22 -0300

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split('');
+
+    let reversedString = splitString.reverse().join("");
+
+    return reversedString;
 
 };
 
