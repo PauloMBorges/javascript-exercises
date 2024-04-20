42826138197b926fba1ec8dd74ec436d6459ddb5
PauloMBorges <paulodemeloborges@gmail.com>
2024-04-19 22:57:20 -0300

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+    
+    return array.filter(element => {
+        const shouldBeExcluded = elementsToRemove.includes(element); // checks if current element should be excluded
+        return !shouldBeExcluded; // if it should not be excluded, includes it in the new array
+    });
 };
 
 // Do not edit below this line
