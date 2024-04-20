d9cb36b852bde48c37a78b7d9d885fb92438af96
PauloMBorges <paulodemeloborges@gmail.com>
2024-04-19 23:35:57 -0300

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+function isNotNumber(value) {
+    return typeof value !== 'number';
+}
+
+const sumAll = function(startValue, finishValue) {
+
+    if (startValue > finishValue) {
+        let aux = startValue;
+        startValue = finishValue;
+        finishValue = aux;
+    }
+
+    if (startValue < 0 || finishValue < 0 ||
+        isNotNumber(startValue) || isNotNumber(finishValue)) {
+        return "ERROR";
+    }
+
+    let result = 0;
+
+    for(let i = startValue; i <= finishValue; i++) {
+        result += i;
+    }
+
+    return result;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
