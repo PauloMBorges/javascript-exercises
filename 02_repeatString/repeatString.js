291b753b208b8c2e5c822e524c92e76ae41e759f
PauloMBorges <paulodemeloborges@gmail.com>
2024-04-19 22:09:07 -0300

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    let repeatString = "";
+    
+    if(times < 0) {
+        repeatString = "ERROR";
+    }
 
+    for(let i = 0; i < times; i++) {
+        repeatString += str;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
